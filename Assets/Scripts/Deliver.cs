c9b031cbf626dbcbb405e6aec3e0a8a7e0da43de
Noman5237 <anonyman637@gmail.com>
2020-11-18 18:11:29 +0600

    Box Delivery Complete

--- Assets/Scripts/Deliver.cs
+++ Assets/Scripts/Deliver.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class Deliver : MonoBehaviour {
 	private bool _delivered;
 
-	private void OnCollisionEnter2D(Collision2D other) {
+	private void OnTriggerEnter2D(Collider2D other) {
 		if (_delivered) {
 			return;
 		}
 
-		if (other.gameObject.CompareTag("Car")) {
+		if (other.gameObject.CompareTag("Load")) {
 			Debug.Log("Delivery Complete");
 			_delivered = true;
+			GameObject o;
+			(o = other.gameObject).GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+			o.transform.position = gameObject.transform.position;
+			o.transform.rotation = Quaternion.identity;
+			other.gameObject.GetComponent<BoxCollider2D>().enabled = false;
 		}
 	}
 }
